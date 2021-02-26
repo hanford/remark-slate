b0299df721425f72708f9198a4693ef9cf80c246
David Govea <david.govea@lattice.com>
2021-02-25 17:13:18 -0800

    tests: update test & snapshot with latest changes

--- test/serialize/serialize-thematic-break.test.ts
+++ test/serialize/serialize-thematic-break.test.ts
@@ -4,7 +4,7 @@ it('Serialize a heading one from slate state to markdown', () => {
   expect(
     serialize({
       type: defaultNodeTypes.thematic_break,
-      thematicBreak: true,
+      children: [{ text: '' }],
     })
   ).toMatchSnapshot();
 });
