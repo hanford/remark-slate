7c754056ff99ae8fbcbf7f5b1bb6e8b2b7f8fad7
David Govea <david.govea@lattice.com>
2021-02-26 09:25:47 -0800

    chore: copypasta test title

--- test/serialize/serialize-thematic-break.test.ts
+++ test/serialize/serialize-thematic-break.test.ts
@@ -1,6 +1,6 @@
 import { serialize, defaultNodeTypes } from '../../src';
 
-it('Serialize a heading one from slate state to markdown', () => {
+it('Serialize a thematic break from slate state to markdown', () => {
   expect(
     serialize({
       type: defaultNodeTypes.thematic_break,
