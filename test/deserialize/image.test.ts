46e023e25676db1527fcfb7f34f05239bf43d22d
Samson Zhang <wwsamson@yahoo.com>
2021-04-14 16:14:13 -0400

    implement images

---
+++ test/deserialize/image.test.ts
@@ -0,0 +1,12 @@
+import { deserialize } from '../../src';
+
+it('deserialize image', () => {
+  expect(
+    deserialize({
+      type: 'image',
+      url: 'https://avatars.githubusercontent.com/u/2148168',
+      alt: 'Jack\'s profile picture',
+      children: [{ text: '' }],
+    })
+  ).toMatchSnapshot();
+});
---
+++ test/serialize/serialize-image.test.ts
@@ -0,0 +1,12 @@
+import { serialize, defaultNodeTypes } from '../../src';
+
+it('Serialize a image from slate state to markdown', () => {
+  expect(
+    serialize({
+      type: defaultNodeTypes.image,
+      link: 'https://avatars.githubusercontent.com/u/2148168',
+      caption: 'Jack\'s profile picture',
+      children: [],
+    })
+  ).toMatchSnapshot();
+});
