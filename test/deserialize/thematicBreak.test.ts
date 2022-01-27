041fff4989c84f5de7f165eed86f963d83e013a1
David Golightly <david.golightly@masterclass.com>
2022-01-27 09:58:02 -0800

    fix ts problems in tests

--- test/deserialize/thematicBreak.test.ts
+++ test/deserialize/thematicBreak.test.ts
@@ -1,8 +1,8 @@
-import { deserialize } from '../../src';
+import { deserialize, MdastNode } from '../../src';
 import thematicBreakast from '../fixtures/thematic-break';
 
 it('When the list has thematic breaks', () => {
   expect(
-    thematicBreakast.children.map((k) => deserialize(k))
+    thematicBreakast.children.map((k) => deserialize(k as MdastNode))
   ).toMatchSnapshot();
 });
